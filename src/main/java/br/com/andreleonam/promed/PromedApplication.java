6f11cdc192cf4d9f79c544aa2d72af7e6fcdb341
André Costa <andreleonam@hotmail.com>
2026-03-29 03:06:04 -0300

    feat: adiciona telefones e email à entidade Pessoa

--- src/main/java/br/com/andreleonam/promed/PromedApplication.java
+++ src/main/java/br/com/andreleonam/promed/PromedApplication.java
@@ -1,46 +1,12 @@
 package br.com.andreleonam.promed;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.andreleonam.promed.domain.Exam;
-import br.com.andreleonam.promed.domain.Medicine;
-import br.com.andreleonam.promed.repositories.ExamRepository;
-import br.com.andreleonam.promed.repositories.MedicineRepository;
-
 @SpringBootApplication
-public class PromedApplication implements CommandLineRunner {
+public class PromedApplication {
 
-	@Autowired
-	private ExamRepository examRepository;
-	
-	@Autowired
-	private MedicineRepository medicineRepository;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(PromedApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Exam ex1 = new Exam(null, "Hemograma", "HEM");
-		Exam ex2 = new Exam(null, "Albumina", "ALB");
-		Exam ex3 = new Exam(null, "Amilase", "AMI");
-	
-		examRepository.saveAll(Arrays.asList(ex1, ex2, ex3));
-		
-		Medicine med1 = new Medicine(null, "Paracetamol 750mg");
-		Medicine med2 = new Medicine(null, "Dipirona");
-		Medicine med3 = new Medicine(null, "Sulfassalazina 500mg");
-	
-		medicineRepository.saveAll(Arrays.asList(med1, med2, med3));
-		
-		
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PromedApplication.class, args);
+    }
 }
